a872de83929911590b38134d6465097a32762a17
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-11-25 06:45:41 -0400

    Aula 7. Propriedades e métodos - Exercício: Cálculo da idade

---
+++ pooCalculaIdade/pooCalculaIdade/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooCalculaIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que calcula a Idade de uma pessoa");
+            Pessoa p = new Pessoa();
+            Console.WriteLine("Nome da Pessoa: ");
+            p.Nome = Console.ReadLine();
+            Console.WriteLine("Ano de nascimento: ");
+            p.AnoNascimento = Convert.ToInt32(Console.ReadLine());
+            p.ExibirDados();
+            Console.ReadKey();
+        }
+    }
+}
