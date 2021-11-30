cb53e9d9f721716376a7207c34469641de3daaca
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-11-30 07:50:07 -0400

    Aula 12. Resolução do Exercício Maior Idade

---
+++ maiorIdade/maiorIdade/Pessoa.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maiorIdade
+{
+    class Pessoa
+    {
+        private String nome;
+
+        public String Nome
+        {
+            get { return nome; }
+            set { nome = value.ToUpper(); }
+        }
+
+        private int anoNascimento;
+
+        public int AnoNascimento
+        {
+            get { return anoNascimento; }
+            set { anoNascimento = value; }
+        }
+
+        private int idade;
+
+        public int Idade
+        {
+            get {
+                this.CalcularIdade();
+                return idade; }
+         
+        }
+
+        public void CalcularIdade()
+        {
+            DateTime dataAtual = DateTime.Now;
+            int ano = dataAtual.Year;
+
+            this.idade = ano - this.anoNascimento;
+        }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine("Pessoa mais Velha!");
+            Console.WriteLine("Nome: " + this.nome);
+            Console.WriteLine("Idade: " + this.idade);
+
+        }
+
+
+
+    }
+}
---
+++ maiorIdade/maiorIdade/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maiorIdade
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa();
+            Pessoa pessoa2 = new Pessoa();
+            Pessoa pessoa3 = new Pessoa();
+
+            Console.Write("Informe o nome: ");
+            pessoa1.Nome = Console.ReadLine();
+            Console.Write("Informe o ano de nascimento: ");
+            pessoa1.AnoNascimento = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Informe o nome: ");
+            pessoa2.Nome = Console.ReadLine();
+            Console.Write("Informe o ano de nascimento: ");
+            pessoa2.AnoNascimento = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Informe o nome: ");
+            pessoa3.Nome = Console.ReadLine();
+            Console.Write("Informe o ano de nascimento: ");
+            pessoa3.AnoNascimento = Convert.ToInt32(Console.ReadLine());
+
+            if (pessoa1.Idade > pessoa2.Idade && pessoa1.Idade > pessoa3.Idade)
+            {
+                pessoa1.ExibirDados();
+            }else if (pessoa2.Idade > pessoa1.Idade && pessoa2.Idade > pessoa3.Idade)
+            {
+                pessoa2.ExibirDados();
+            }
+            else
+            {
+                pessoa3.ExibirDados();
+            }
+
+            //if (pessoa1.Idade >= 18)
+            //{
+            //    Console.WriteLine("é de Maior");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Não é de Maior");
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
