29c3899bda35148a3aabcbdd7d1c275b1ebaf32f
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-11-30 07:11:24 -0400

    Aula 11. Resolução do Exercício Controle de animais

---
+++ controleDeAnimais/controleDeAnimais/Animais.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace controleDeAnimais
+{
+     public class Animais
+    {
+        #region Construtor
+        //public Animais(String nomeAnimal, String tipoAnimal)
+        //{
+        //    this.nome = nomeAnimal;
+        //    if (tipoAnimal == "Cachorro" || tipoAnimal == "Gato" || tipoAnimal == "Peixe")
+        //    {
+        //        tipo = tipoAnimal;
+        //    }
+        //    else
+        //    {
+        //        tipo = "Peixe";
+        //    }
+        //}
+        #endregion
+
+        #region Atributos
+        private String nome;
+
+        public String Nome
+        {
+            get { return nome; }
+            set { nome = value.ToUpper(); }
+        }
+        private String tipo;
+
+        public String Tipo
+        {
+            get { return tipo; }
+            set
+            {
+                if (value == "Cachorro" || value == "Gato" || value == "Peixe")
+                {
+                    tipo = value;
+                }
+                else
+                {
+                    tipo = "Peixe";
+                }
+            }
+        }
+        #endregion
+        //public int quantidadeCachorros
+        //{
+        //    get;
+        //    set;
+        //}
+
+        //public int quantidadeGatos
+        //{
+        //    get;
+        //    set;
+        //}
+
+        //public int quantidadePeixes
+        //{
+        //    get;
+        //    set;
+        //}
+        //
+
+        //#region Métodos
+        //public void quantidadeAnimais()
+        //{
+        //    Console.WriteLine("Quantidade de Cachorros: " + this.quantidadeCachorros);
+        //    Console.WriteLine("Quantidade de Gatos: " + this.quantidadeGatos);
+        //    Console.WriteLine("Quantidade de Peixes: " + this.quantidadePeixes);
+        //}
+        //#endregion
+    }
+}
---
+++ controleDeAnimais/controleDeAnimais/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace controleDeAnimais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countCachorro = 0, countGato = 0, countPeixe = 0;
+            Animais animal1 = new Animais();
+            Animais animal2 = new Animais();
+            Animais animal3 = new Animais();
+            Animais animal4 = new Animais();
+            Animais animal5 = new Animais();
+
+            Console.WriteLine("Informe o Nome do Animal: ");
+            //String nomeAnimal1 = Console.ReadLine();
+            animal1.Nome = Console.ReadLine();
+            Console.WriteLine("Informe o Tipo do Animal: ");
+            //String tipoAnimal1 = Console.ReadLine();
+            animal1.Tipo = Console.ReadLine();
+            //Animais animal1 = new Animais(nomeAnimal1, tipoAnimal1);
+            if (animal1.Tipo == "Cachorro") countCachorro++;
+            if (animal1.Tipo == "Gato") countGato++;
+            if (animal1.Tipo == "Peixe") countPeixe++;
+            //Console.WriteLine(animal1.Nome + animal1.Tipo);
+
+            Console.WriteLine("Informe o Nome do Animal: ");
+            //String nomeAnimal2 = Console.ReadLine();
+            animal2.Nome = Console.ReadLine();
+            Console.WriteLine("Informe o Tipo do Animal: ");
+            //String tipoAnimal2 = Console.ReadLine();
+            animal2.Tipo = Console.ReadLine();
+            //Animais animal2 = new Animais(nomeAnimal2, tipoAnimal2);
+            if (animal2.Tipo == "Cachorro") countCachorro++;
+            if (animal2.Tipo == "Gato") countGato++;
+            if (animal2.Tipo == "Peixe") countPeixe++;
+            //Console.WriteLine(animal2.Nome, animal2.Tipo);
+
+            Console.WriteLine("Informe o Nome do Animal: ");
+            //String nomeAnimal3 = Console.ReadLine();
+            animal3.Nome = Console.ReadLine();
+            Console.WriteLine("Informe o Tipo do Animal: ");
+            //String tipoAnimal3 = Console.ReadLine();
+            animal3.Tipo = Console.ReadLine();
+            //Animais animal3 = new Animais(nomeAnimal3, tipoAnimal3);
+            if (animal3.Tipo == "Cachorro") countCachorro++;
+            if (animal3.Tipo == "Gato") countGato++;
+            if (animal3.Tipo == "Peixe") countPeixe++;
+
+            Console.WriteLine("Informe o Nome do Animal: ");
+            //String nomeAnimal4 = Console.ReadLine();
+            animal4.Nome = Console.ReadLine();
+            Console.WriteLine("Informe o Tipo do Animal: ");
+            //String tipoAnimal4 = Console.ReadLine();
+            animal4.Tipo = Console.ReadLine();
+            //Animais animal4 = new Animais(nomeAnimal4, tipoAnimal4);
+            if (animal4.Tipo == "Cachorro") countCachorro++;
+            if (animal4.Tipo == "Gato") countGato++;
+            if (animal4.Tipo == "Peixe") countPeixe++;
+
+            Console.WriteLine("Informe o Nome do Animal: ");
+            //String nomeAnimal5 = Console.ReadLine();
+            animal5.Nome = Console.ReadLine();
+            Console.WriteLine("Informe o Tipo do Animal: ");
+            //String tipoAnimal5 = Console.ReadLine();
+            animal5.Tipo = Console.ReadLine();
+            //Animais animal5 = new Animais(nomeAnimal5, tipoAnimal5);
+            if (animal5.Tipo == "Cachorro") countCachorro++;
+            if (animal5.Tipo == "Gato") countGato++;
+            if (animal5.Tipo == "Peixe") countPeixe++;
+
+            Console.WriteLine("Total de Animais!");
+            Console.WriteLine("Quantidade de Cachorros: " + countCachorro);
+            Console.WriteLine("Quantidade de Gatos: " + countGato);
+            Console.WriteLine("Quantidade de Peixes: " + countPeixe);
+
+            Console.ReadKey();
+
+        }
+    }
+}
