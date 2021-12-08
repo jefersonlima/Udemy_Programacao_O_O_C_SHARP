19033a1949e2b7bde90cb60b3979a1bfc5ab01d5
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-12-08 07:59:12 -0400

    Aula 13. Resolução do exercício área de um retângulo

---
+++ areaQuadradoRetangulo/areaQuadradoRetangulo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace areaQuadradoRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuadradoRetangulo qr1 = new QuadradoRetangulo();
+
+            Console.Write("Informe a Base do Quadrado Retangulo: ");
+            qr1.BaseQuadrado = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Informe a Altura do Quadrado Retangulo: ");
+            qr1.AlturaQuadrado = Convert.ToDouble(Console.ReadLine());
+
+            qr1.ExibirAreaQuadradoRetangulo();
+            qr1.ExibirTodosDadosQuadradoRetangulo();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ areaQuadradoRetangulo/areaQuadradoRetangulo/QuadradoRetangulo.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace areaQuadradoRetangulo
+{
+    class QuadradoRetangulo
+    {
+        #region Contrutor
+        public QuadradoRetangulo()
+        {
+            this.baseQuadrado = 0;
+            this.alturaQuadrado = 0;
+        }
+        #endregion
+
+        #region Propriedades
+
+        #region Base do Quadrado Retangulo
+        private Double baseQuadrado;
+
+        public Double BaseQuadrado
+        {
+            get { return baseQuadrado; }
+            set {
+                if (value >= 0) this.baseQuadrado = value;
+                else this.baseQuadrado = 0; 
+            }
+        }
+        #endregion
+
+        #region Altura do Quadrado Retangulo
+        private Double alturaQuadrado;
+
+        public Double AlturaQuadrado
+        {
+            get { return alturaQuadrado; }
+            set {
+                if (value >= 0) this.alturaQuadrado = value;
+                else this.alturaQuadrado = 0; 
+            }
+        }
+        #endregion
+
+        #region Area do Quadrado Retangulo
+        public Double AreaQuadrado
+        {
+            get { return baseQuadrado * alturaQuadrado; }
+        }
+        #endregion
+
+        #endregion
+
+        #region Metodos
+        #region Exibir Area do Quadrado Retangulo
+        public void ExibirAreaQuadradoRetangulo()
+        {
+            Console.WriteLine("Area do Quadrado Retangulo: " + this.AreaQuadrado);
+            Console.WriteLine();
+        }
+        #endregion
+
+        #region Exibir todos dados do Quadrado Retangulo
+        public void ExibirTodosDadosQuadradoRetangulo()
+        {
+            Console.WriteLine("Base do Quadrado Retangulo: " + this.baseQuadrado);
+            Console.WriteLine("Altura do Quadrado Retangulo: " + this.alturaQuadrado);
+            Console.WriteLine("Area do Quadrado Retangulo: " + this.AreaQuadrado);
+            Console.WriteLine();
+        }
+        #endregion
+
+        #endregion
+    }
+}
