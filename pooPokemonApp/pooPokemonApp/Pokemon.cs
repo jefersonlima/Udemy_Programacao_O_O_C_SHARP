496a25644d01769eb89741d3c62630a6d585e0a3
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-12-14 07:01:02 -0400

    Aula 14. Criando a nossa Pokédex

---
+++ pooPokemonApp/pooPokemonApp/Pokemon.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooPokemonApp
+{
+    public class Pokemon
+    {
+        #region Construtor
+        public Pokemon()
+        {
+            this.Nome = "";
+            this.Descricao = "";
+        }
+        #endregion
+
+        #region Atributos Pokemons
+        private String nome;
+
+        public String Nome
+        {
+            get { return this.nome; }
+            set { this.nome = value.ToUpper(); }
+        }
+
+        private String descricao;
+
+        public String Descricao
+        {
+            get { return this.descricao; }
+            set { this.descricao = value.ToUpper(); }
+        }
+        #endregion
+
+        #region Metodos
+        public void ExibirDadosPokemon()
+        {
+            Console.WriteLine("Nome do Pokémon: " + this.Nome);
+            Console.WriteLine(" Descrição do Pokémon: " + this.Nome + " " + this.Descricao);
+        }
+        #endregion
+    }
+}
---
+++ pooPokemonApp/pooPokemonApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooPokemonApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pokemon p = new Pokemon();
+
+            p.Nome = "Bulbassauro";
+            p.Descricao = "é um pokemon tipo planta...";
+
+            p.ExibirDadosPokemon();
+
+            Console.ReadKey();
+        }
+    }
+}
