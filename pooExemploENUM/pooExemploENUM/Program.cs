21cfa232172c0bc714ab27be06ad7a3b263b929a
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-12-14 13:57:37 -0400

    Aula 19. Apresentação do tipo de dados ENUM

---
+++ pooExemploENUM/pooExemploENUM/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+enum mes { Janeiro, Fevereiro, Marco, Abril, Maio, junho, Julho, Agosto, Setembro, Outubro, Novembro, Dezembro}
+
+namespace pooExemploENUM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mes mesAniversario;
+
+            //Jeito 1
+            mesAniversario = (mes)2;
+            Console.WriteLine("O meu aniversario é no mês de " + mesAniversario);
+
+            //Jeito 2
+            mesAniversario = mes.Dezembro;
+            Console.WriteLine("O meu aniversario é no mês de " + mesAniversario);
+
+            Console.ReadKey();
+        }
+    }
+}
