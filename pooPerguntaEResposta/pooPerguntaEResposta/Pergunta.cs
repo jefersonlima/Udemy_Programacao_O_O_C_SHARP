597281a81a9e320eecfbda45cb5c29840fdd6f85
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-12-14 11:22:59 -0400

    Aula 18. Propriedades simplificadas

---
+++ pooPerguntaEResposta/pooPerguntaEResposta/Pergunta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooPerguntaEResposta
+{
+    public class Pergunta
+    {
+        #region Construtor
+        public Pergunta()
+        {
+            this.Texto = "";
+            this.Resposta = "";
+        }
+
+        public Pergunta(String texto, String resposta)
+        {
+            this.Texto = texto;
+            this.Resposta = resposta;
+        }
+        #endregion
+
+        #region Atributos
+        public String Texto { get; set; }
+        public String Resposta { get; set; }
+        #endregion
+
+    }
+}
