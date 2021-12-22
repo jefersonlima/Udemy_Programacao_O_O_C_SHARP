f6f74dbbfd4fd5b1333901b516d86c75c60e08ed
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-12-22 08:02:28 -0400

    Aula 29. Destrutores

---
+++ pooDestruidores/pooDestruidores/Ferramenta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooDestruidores
+{
+    public class Ferramenta
+    {
+        public Ferramenta( String nome)
+        {
+            this.Nome = nome;
+        }
+
+        ~Ferramenta()
+        {
+            Console.WriteLine("A sua ferramenta " + this.Nome + " Foi Destruida!");
+        }
+        public String Nome { get; set; }
+    }
+}
---
+++ pooDestruidores/pooDestruidores/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooDestruidores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ferramenta f1 = new Ferramenta("Martelo");
+            Ferramenta f2 = new Ferramenta("Martelete");
+
+            f1 = null;
+            f2 = new Ferramenta("Marreta");
+
+            GC.Collect();
+            Console.ReadKey();
+        }
+    }
+}
