fb45cce54bcb755c1c71dd64cd035ebfc5954acc
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-12-23 05:40:51 -0400

    Aula 30. Modificador Static - Meu próprio Console.class

---
+++ pooStatic/pooStatic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooStatic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string nome;
+            ////ModoTexto.Escrever("Qual é o seu nome?");
+            ////nome = ModoTexto.Ler();
+            //nome = ModoTexto.Ler("Qual é o seu nome?");
+            //ModoTexto.Escrever("Olá " + nome);
+            //ModoTexto.Pausar();
+
+            string nome = "Jeferson Lima";
+            ModoTexto.log = nome;
+            ModoTexto.Escrever("Olá " + ModoTexto.log);
+            ModoTexto.Pausar();
+        }
+    }
+}
