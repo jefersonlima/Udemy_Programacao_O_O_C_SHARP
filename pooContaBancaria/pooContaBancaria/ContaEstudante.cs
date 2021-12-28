874954ae3ec906316c268662d378a346c3ed34c3
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-12-28 07:10:20 -0400

    Aula 40. Conta bancária - Vídeo resolução exercício

---
+++ pooContaBancaria/pooContaBancaria/ContaEstudante.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooContaBancaria
+{
+    public class ContaEstudante:ContaBancaria
+    {
+        public ContaEstudante():base()
+        {
+            this.limite = 500;
+        }
+        private Double limite;
+
+        public Double Limite
+        {
+            get { return this.limite; }
+            
+        }
+        public override Double Retirar(Double valor)
+        {
+            //Retirar apenas se tiver saldo
+            if (this.saldo + this.limite >= valor)
+            {
+                this.saldo -= valor;
+            }
+            else
+            {
+                throw new Exception("Saldo insuficiente!");
+            }
+            return this.saldo;
+        }
+
+        public void ExibirDadosBancarios()
+        {
+            Console.WriteLine("Dados do correntista: " + this.Correntista.Nome);
+            Console.WriteLine("Saldo: " + this.Saldo);
+            Console.WriteLine("Limite: " + this.Limite);
+        }
+
+    }
+}
---
+++ pooContaBancaria/pooContaBancaria/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ContaBancaria conta = new ContaBancaria();
+            //conta.Depositar(600);
+            //conta.Correntista.Nome = "Jeferson Lima";
+
+            ContaEstudante conta = new ContaEstudante();
+            conta.Correntista.Nome = "Jeferson Lima";
+            int menu = 1;
+
+            while (menu != 0)
+            {
+                Console.WriteLine("Menu de opções");
+                Console.WriteLine("1 - Exibir dados do correntista");
+                Console.WriteLine("2 - Depositar");
+                Console.WriteLine("3 - Retirar");
+                Console.WriteLine("0 - Sair");
+                Console.Write("Escolha uma opção: ");
+                menu = Convert.ToInt32(Console.ReadLine());
+
+                if (menu == 1)
+                {
+                    conta.ExibirDadosBancarios();
+                }
+                else if (menu == 2)
+                {
+                    try
+                    {
+                        Console.Write("Valor para Deposito: ");
+                        Double valor = Convert.ToDouble(Console.ReadLine());
+                        conta.Depositar(valor);
+                        Console.WriteLine("Saldo atual: " + conta.Saldo);
+
+                    }
+                    catch (Exception erro)
+                    {
+
+                        Console.WriteLine("Ops!!! Algo deu errado.");
+                        Console.WriteLine("Erro: " + erro.Message);
+                    }
+                }
+                else if (menu == 3)
+                {
+                    try
+                    {
+                        Console.Write("Valor para retirada: ");
+                        Double valor = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Saldo restante: " + conta.Retirar(valor));
+
+                    }
+                    catch (Exception erro)
+                    {
+
+                        Console.WriteLine("Ops!!! Algo deu errado.");
+                        Console.WriteLine("Erro: " + erro.Message);
+                    }
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
