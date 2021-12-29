3a7a2224b54e926a46828b1806a86efcb3e282e7
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-12-28 22:23:22 -0400

    Aula 44. Classe Lista de jogos - Excluir - Alterar - Listar e Localizar

--- pooControleDeJogos/pooControleDeJogos/ListaDeJogos.cs
+++ pooControleDeJogos/pooControleDeJogos/ListaDeJogos.cs
@@ -19,13 +19,13 @@ namespace pooControleDeJogos
         }
         /*
          //* Cadastrar um jogo;
-         * Excluir um jogo;
-         * Alterar um jogo;
-         * Localizar jogod por nome;
+         //* Excluir um jogo;
+         //* Alterar um jogo;
+         //* Localizar jogos por nome;
          * Localizar jogos por gênero;
          * Listar todos os jogos;
-         * Listar todos os jogos por gêneto;
-         * Listar todos os jogos por console;
+         //* Listar todos os jogos por gêneto;
+         //* Listar todos os jogos por console;
          */
 
         public ListaDeJogos()
@@ -54,6 +54,61 @@ namespace pooControleDeJogos
                 resultado = false;
             }
             return resultado;
+        }
+
+        public Boolean Alterar(Jogo jogo)
+        {
+            Boolean resultado = true;
+
+            try
+            {
+                Jogo j = jogos.Find(x => x.Id == jogo.Id);
+                if (j != null)
+                {
+                    j.Nome = jogo.Nome;
+                    j.Descricao = jogo.Descricao;
+                    j.Genero = jogo.Genero;
+                    j.Console = jogo.Console;
+                }
+            }
+            catch (Exception)
+            {
+
+                resultado = false;
+            }
+
+            return resultado;
+        }
+
+        public Boolean Excluir (int id)
+        {
+            Boolean resultado = false;
+
+            Jogo j = jogos.Find(x => x.Id == id);
+            if (j != null)
+            {
+                resultado = jogos.Remove(j);
+            }
+
+            return resultado;
+        }
+
+        public List<Jogo> Localizar(String nome)
+        {
+            List<Jogo> lj = jogos.FindAll(x => x.Nome.Contains(nome.ToUpper()));
+            return lj;
+        }
+
+        public List<Jogo> ListarPorGenero(TipoGenero genero)
+        {
+            List<Jogo> lj = jogos.FindAll(x => x.Genero.Equals(genero));
+            return lj;
+        }
+
+        public List<Jogo> ListarPorConsole(TipoConsole console)
+        {
+            List<Jogo> lj = jogos.FindAll(x => x.Console.Equals(console));
+            return lj;
         }
     }
 }
