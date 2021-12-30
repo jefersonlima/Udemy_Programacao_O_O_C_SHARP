2ee739685651003881a95b58b771977a16d43a08
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-12-30 08:01:49 -0400

    Aula 52. Construindo o gerenciador de matérias, 51. Interfaces X Classes Abstratas

---
+++ pooControleDeNotas/pooControleDeNotas/GerenciadorDeMaterias.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooControleDeNotas
+{
+    public class GerenciadorDeMaterias
+    {
+        public GerenciadorDeMaterias()
+        {
+            this.Estudante = new Aluno();
+            this.materias = new List<Materia>();
+        }
+        public GerenciadorDeMaterias(String nome, String email)
+        {
+            this.Estudante = new Aluno(nome, email);
+            this.materias = new List<Materia>();
+        }
+        public Aluno Estudante { get; set; }
+
+        private List<Materia> materias;
+
+        public List<Materia> Materias 
+        {
+            get
+            {
+                return this.materias;
+            }               
+        }
+
+        #region Metodos
+        public void CadastrarMaterias(Materia materia)
+        {
+            try
+            {
+                materias.Add(materia);
+            }
+            catch (Exception erro)
+            {
+                throw new Exception("Erro ao cadastrat matéria");
+            }
+        }
+
+        public void ListarMaterias()
+        {
+            Console.WriteLine("**** Matérias ****");
+            for (int i = 0; i < materias.Count; i++)
+            {
+                Materia m = materias[i];
+                m.ExibirDados();
+                Console.WriteLine("********");
+            }
+        }
+        #endregion
+    }
+}
--- pooControleDeNotas/pooControleDeNotas/Program.cs
+++ pooControleDeNotas/pooControleDeNotas/Program.cs
@@ -10,20 +10,35 @@ namespace pooControleDeNotas
     {
         static void Main(string[] args)
         {
-            int op = 1;
-            Aluno aluno = new Aluno("Jeferson", "jeferson@gmail.com");
-            aluno.ExibirDados();
-            Console.WriteLine("Controle de Matérias");           
-            while (op!= 0)
+            int op = 0;
+            String NomeMateria = "";
+            Double n1 = 0, n2 = 0, mo = 5;
+            Materia materia;
+
+            GerenciadorDeMaterias gm = new GerenciadorDeMaterias("Jeferson", "jeferson@gmail.com");
+                     
+            while (op!= 5)
             {
+                Console.WriteLine("Controle de Matérias");
+                gm.Estudante.ExibirDados();
                 op = ExibirMenu();
-                if(op == 1)
+                Console.Clear();
+                if (op == 1)
                 {//Exibir matérias
-
+                    gm.ListarMaterias();
                 }
-                else if(op == 2)
+                else if (op == 2)
                 {//Cadastrar matérias
+                    Console.WriteLine("Cadastro de Matérias");
+                    Console.Write("Materia: ");
+                    NomeMateria = Console.ReadLine();
+                    Console.Write("1º nota: ");
+                    n1 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("2º nota: ");
+                    n2 = Convert.ToDouble(Console.ReadLine());
 
+                    materia = new Materia(NomeMateria, n1, n2, mo);
+                    gm.CadastrarMaterias(materia);
                 }
                 else if (op == 3)
                 {//Alterar matérias
@@ -33,24 +48,25 @@ namespace pooControleDeNotas
                 {//Excluir matérias
 
                 }
+                Console.ReadKey();
             }
-
             Console.ReadKey();
         }
 
         static int ExibirMenu()
         {
-            int op = 1;
+            int op = 0;
             try
             {
-                while (op != 0)
-                //while (op < 0 && op > 4)
+                //while (op != 0)
+                while (op < 1 || op > 5
+                    )
                 {
                     Console.WriteLine("1 - Exibir matérias");
                     Console.WriteLine("2 - Cadastrar matérias");
                     Console.WriteLine("3 - Alterar dados das matérias");
                     Console.WriteLine("4 - Excluir dados das matérias");
-                    Console.WriteLine("0 - Sair");
+                    Console.WriteLine("5 - Sair");
                     Console.Write("Opção: ");
 
                     op = Convert.ToInt32(Console.ReadLine());
