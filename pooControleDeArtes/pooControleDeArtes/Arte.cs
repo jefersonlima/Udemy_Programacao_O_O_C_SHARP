5878d8c1958d5d8f56d19c402be48b9879c434d0
JefersonLima <jefersonlimadesouza@hotmail.com.br>
2021-12-31 06:11:41 -0400

    Aula 55. Cadastrando as minhas obras de arte

---
+++ pooControleDeArtes/pooControleDeArtes/Arte.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooControleDeArtes
+{
+    public class Arte : IArte
+    {
+        public Arte()
+        {
+            this.Nome = "";
+            this.Descricao = "";
+        }
+        public Arte(String nome, String descricao)
+        {
+            this.Nome = nome;
+            this.Descricao = descricao;
+        }
+
+        private String nome;
+
+        public String Nome
+        {
+            get { return this.nome; }
+            set { this.nome = value.ToUpper(); }
+        }
+
+        private String descricao;
+
+        public String Descricao
+        {
+            get { return this.descricao; }
+            set { this.descricao = value.ToUpper(); }
+        }
+
+        public void ExibirDados()
+        {
+            //Console.WriteLine("Arte: " + this.Nome + " Descrição: " + this.Descricao);
+            Console.WriteLine("Arte: {0} - Descrição: {1}", this.Nome, this.Descricao);
+        }
+    }
+}
---
+++ pooControleDeArtes/pooControleDeArtes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooControleDeArtes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Arte> Artes = new List<Arte>();
+            int n = 0;
+            Arte arte;
+
+            Console.WriteLine("Controle de obras de arte");
+            Console.Write("Quantas obras você quer inserir: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Iniciando o cadastro das obras de arte");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Leitura da Obra {0}", i);
+                arte = new Arte();
+                Console.Write("Nome: ");
+                arte.Nome = Console.ReadLine();
+                Console.Write("Descrição: ");
+                arte.Descricao = Console.ReadLine();
+
+                Artes.Add(arte);
+            }
+            Console.WriteLine("lista das obras de artes cadasradas");
+            
+            Console.WriteLine("mostrando com for");
+            for (int i = 0; i < n; i++)               
+            {
+                Artes[i].ExibirDados();             
+            }
+            Console.WriteLine("mostrando com foreach");
+            foreach (var item in Artes)
+            {
+                item.ExibirDados();
+            }
+            Console.ReadKey();
+        }
+    }
+}
